f341f590ba66d1e83529006a690ac03f037f8e64
pranav <pranavkelkar@protonmail.com>
2022-05-10 08:19:54 +0530

    Change title and icon

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# remove old html pages
+# remove old site
 rm *.html
 
 # update build date
@@ -9,6 +9,11 @@ sed -i "s/^<p>Last Updated.*/<p>Last Updated: $BUILD_DATE<\/p>/g" includes/foote
 
 # build new html pages with pandoc
 find . -name \*.md -type f -exec pandoc -c includes/style.css -B includes/header.html -A includes/footer.html -o {}.html {} \;
+
+# rename from md.html to html
 find . -depth -name '*.md.html' -execdir bash -c 'mv -i "$1" "${1//md.html/html}"' bash {} \;
 
+# publish the site
+# github: at this point if the changes are pushed, changes will be automatically published
+# lo
 # done
